017273fe2975ee10f2fbaada3e0f77ec89896ca7
Tycho <tychosteur@gmail.com>
2024-05-06 21:53:30 +0200

    null lines in hexdumps are now compacted into a single '*'

--- src/hexdump.c
+++ src/hexdump.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdbool.h>
 #include <ctype.h>
 #include <stdio.h>
 
@@ -6,31 +7,48 @@
 
 #define DUMP_WIDTH 16
 
+bool is_null_line(void* line_start, size_t length_left) {
+    for (size_t i = 0; i < length_left && i < DUMP_WIDTH; i++) {
+        uint8_t byte = ((uint8_t*)line_start)[i];
+        if (byte != 0)
+            return (false);
+    }
+    return (true);
+}
+
+void dump_line(void* line_start, size_t length_left) {
+    size_t i;
+    for (i = 0; i < DUMP_WIDTH && i < length_left; i++) {
+        uint8_t byte = ((uint8_t*)line_start)[i];
+        printf("%02x", byte);
+        if (i & 1) printf(" ");
+    }
+    for (; i < DUMP_WIDTH; i++) {
+        printf("  ");
+        if (i & 1) printf(" ");
+    }
+    printf(" ");
+    for (i = 0; i < DUMP_WIDTH && i < length_left; i++) {
+        uint8_t byte = ((uint8_t*)line_start)[i];
+        if (isprint(byte) && byte)
+            printf("%c", byte);
+        else
+            printf(".");
+    }
+}
+
 void hexdump(void* start, size_t length) {
-    for (size_t i = 0; i < length;) {
+    bool last_was_null_line = false;
+    for (size_t i = 0; i < length; i += DUMP_WIDTH) {
+        if (is_null_line((uint8_t*)start + i, length - i)) {
+            if (!last_was_null_line)
+                printf("*\n");
+            last_was_null_line = true;
+            continue;
+        }
         printf("%#016lx ", i);
-        size_t j;
-        for (j = 0; j < DUMP_WIDTH && i < length; j++) {
-            uint8_t byte = ((uint8_t*)start)[i];
-            printf("%02x", byte);
-            if (i & 1) printf(" ");
-            i++;
-        }
-        for (; j < DUMP_WIDTH; j++) {
-            printf("  ");
-            if (i & 1) printf(" ");
-            i++;
-        }
-        printf(" ");
-        i -= DUMP_WIDTH;
-        for (j = 0; j < DUMP_WIDTH && i < length; j++) {
-            uint8_t byte = ((uint8_t*)start)[i];
-            if (isprint(byte) && byte)
-                printf("%c", byte);
-            else
-                printf(".");
-            i++;
-        }
+        dump_line((uint8_t*)start + i, length - i);
         printf("\n");
+        last_was_null_line = false;
     }
 }
